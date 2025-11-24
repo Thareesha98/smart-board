fb2da28a4903ae7cd4622ae7f5ff970d5938fffc
Themiya <themiyakaluarachchi@gmail.com>
2025-11-24 12:52:22 +0530

    Fix the TailwindConfig

--- front-end/src/pages/HomePage.jsx
+++
@@ -1,9 +0,0 @@
-export function HomePage() {
-    return (
-        <>
-            <div className="bg-gray-900">
-                <h1 className="text-5xl text-red-500">This is Home Page</h1>
-            </div>
-        </>
-    );
-}
--- front-end/src/pages/LoginPage.jsx
+++
@@ -1,9 +0,0 @@
-export function LoginPage() {
-    return (
-        <>
-            <div>
-                <h1 className="text-4xl text-blue-700">This is Login Page</h1>
-            </div>
-        </>
-    );
-}
