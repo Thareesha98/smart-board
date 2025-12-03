4a9b198409eea34ed2de4e4de7c47309dbb794cd
Thareesha <thareesha98>
2025-12-04 04:07:31 +0530

    Fixed some project build failures

---
+++ backend/back/sbms-backend/src/main/java/com/sbms/sbms_backend/mapper/AppointmentMapper.java
@@ -0,0 +1,36 @@
+package com.sbms.sbms_backend.mapper;
+
+import com.sbms.sbms_backend.dto.appointment.AppointmentResponseDTO;
+import com.sbms.sbms_backend.model.Appointment;
+
+public class AppointmentMapper {
+
+    public static AppointmentResponseDTO toDto(Appointment a) {
+        AppointmentResponseDTO dto = new AppointmentResponseDTO();
+
+        dto.setId(a.getId());
+
+        dto.setBoardingId(a.getBoarding().getId());
+        dto.setBoardingTitle(a.getBoarding().getTitle());
+        dto.setBoardingAddress(a.getBoarding().getAddress());
+
+        dto.setStudentId(a.getStudent().getId());
+        dto.setStudentName(a.getStudent().getFullName());
+        dto.setStudentEmail(a.getStudent().getEmail());
+
+        dto.setNumberOfStudents(a.getNumberOfStudents());
+
+        dto.setRequestedStartTime(a.getRequestedStartTime());
+        dto.setRequestedEndTime(a.getRequestedEndTime());
+
+        dto.setOwnerStartTime(a.getOwnerStartTime());
+        dto.setOwnerEndTime(a.getOwnerEndTime());
+
+        dto.setStatus(a.getStatus());
+
+        dto.setStudentNote(a.getStudentNote());
+        dto.setOwnerNote(a.getOwnerNote());
+
+        return dto;
+    }
+}
---
+++ backend/back/sbms-backend/src/main/java/com/sbms/sbms_backend/model/Boarding.java
@@ -0,0 +1,79 @@
+package com.sbms.sbms_backend.model;
+
+
+import jakarta.persistence.*;
+
+import lombok.Data;
+import com.sbms.sbms_backend.model.enums.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.sbms.sbms_backend.common.BaseEntity;
+
+@Data
+@Entity
+@Table(name = "boardings")
+public class Boarding extends BaseEntity {
+
+	//ownerID
+	
+	@ManyToOne
+	@JoinColumn(name = "owner_id")
+	private User owner;
+
+	
+    @Column(nullable = false, length = 150)
+    private String title;
+
+    @Column(nullable = false, length = 500)
+    private String description;
+
+    @Column(nullable = false)
+    private String address;
+    
+    @Column(nullable = false)
+    private BigDecimal pricePerMonth;
+    
+    @Column(nullable = true)
+    private BigDecimal keyMoney;
+    
+    private List<String> imageUrls;
+
+    @Column(nullable = false)
+    private Gender genderType; // MALE/FEMALE/MIXED
+
+    @Column(nullable=false)
+   private int available_slots;
+
+    @ElementCollection
+    private Map<String, Double> nearbyPlaces = new HashMap<>();
+    
+
+    @ElementCollection
+    @CollectionTable(name = "boarding_amenities", joinColumns = @JoinColumn(name = "boarding_id"))
+    @Column(name = "amenity")
+    private List<String> amenities;
+
+    @Column(nullable=false)
+    private Integer maxOccupants;
+    
+    private BoardingType boardingType;
+    
+    @Column(nullable=false)
+    private Status status;
+    
+    private boolean isBosted = false;
+    protected LocalDateTime boostEndDate;
+    
+    
+
+    
+}
+
+
+
+
