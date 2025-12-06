a091bdcddd14724cae107da3d530b5ab48dc14ef
Thareesha <thareesha98>
2025-12-06 17:19:21 +0530

    Added Spring Cloud Kubernates

--- backend/sbms-backend-monolith/src/main/java/com/sbms/sbms_backend/SbmsBackendApplication.java
+++ backend/sbms-backend-monolith/src/main/java/com/sbms/sbms_backend/SbmsBackendApplication.java
@@ -2,10 +2,12 @@ package com.sbms.sbms_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class SbmsBackendApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SbmsBackendApplication.class, args);
 	}
