55aaae972d71f9f01b906f98217e6f9e021c9f87
Thareesha <thareesha98>
2025-12-06 22:41:02 +0530

    Created secrets

--- backend/sbms-backend-monolith/src/main/java/com/sbms/sbms_backend/SbmsBackendApplication.java
+++ backend/sbms-backend-monolith/src/main/java/com/sbms/sbms_backend/SbmsBackendApplication.java
@@ -2,9 +2,6 @@ package com.sbms.sbms_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class SbmsBackendApplication {
