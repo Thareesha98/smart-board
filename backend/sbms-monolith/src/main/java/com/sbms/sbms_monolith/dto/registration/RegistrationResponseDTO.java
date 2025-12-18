16654d5de09a656614a98410b7a31d5781860ba3
Thareesha <thareesha98@gmail.com>
2025-12-18 11:37:17 +0530

    Add RegistrationResponseDTO

---
+++ backend/sbms-monolith/src/main/java/com/sbms/sbms_monolith/dto/registration/RegistrationResponseDTO.java
@@ -0,0 +1,30 @@
+package com.sbms.sbms_monolith.dto.registration;
+
+import java.math.BigDecimal;
+
+import com.sbms.sbms_monolith.model.enums.RegistrationStatus;
+import lombok.Data;
+
+@Data
+public class RegistrationResponseDTO {
+
+    private Long id;
+
+    private Long boardingId;
+    private String boardingTitle;
+
+    private Long studentId;
+    private String studentName;
+    private String studentEmail;
+
+    private int numberOfStudents;
+
+    private RegistrationStatus status;
+
+    private String studentNote;
+    private String ownerNote;
+    private BigDecimal keyMoney;
+    private BigDecimal monthlyPrice;
+    private boolean keyMoneyPaid;
+
+}
