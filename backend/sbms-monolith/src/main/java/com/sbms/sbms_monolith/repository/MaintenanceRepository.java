1923bcf8827ef3ade4b034afa79e84fa7924d2aa
Thareesha <thareesha98@gmail.com>
2025-12-20 01:41:09 +0530

    Add MaintenanceRepository

---
+++ backend/sbms-monolith/src/main/java/com/sbms/sbms_monolith/repository/MaintenanceRepository.java
@@ -0,0 +1,16 @@
+package com.sbms.sbms_monolith.repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sbms.sbms_monolith.model.Maintenance;
+import com.sbms.sbms_monolith.model.User;
+
+import java.util.List;
+
+public interface MaintenanceRepository extends JpaRepository<Maintenance, Long> {
+
+    List<Maintenance> findByStudent(User student);
+
+    List<Maintenance> findByBoarding_Owner(User owner);
+}
