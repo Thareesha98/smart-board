8121eb8c109c053588ce19276ac92b0b9f5c5489
Thareesha <thareesha98@gmail.com>
2025-12-20 22:03:40 +0530

    Update SbmsGatewayApplication

--- backend/sbms-gateway/src/main/java/com/sbms/gateway/sbms_gateway/SbmsGatewayApplication.java
+++ backend/sbms-gateway/src/main/java/com/sbms/gateway/sbms_gateway/SbmsGatewayApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SbmsGatewayApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SbmsGatewayApplication.class, args);
 	}
