a947d9f757575c9acdf00af0ef0e1b491452859e
Thareesha <thareesha98@gmail.com>
2025-12-20 22:04:16 +0530

    Update RegistrationRepository

--- backend/sbms-monolith/src/main/java/com/sbms/sbms_monolith/repository/RegistrationRepository.java
+++ backend/sbms-monolith/src/main/java/com/sbms/sbms_monolith/repository/RegistrationRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface RegistrationRepository extends JpaRepository<Registration, Long> {
 
@@ -19,6 +20,8 @@ public interface RegistrationRepository extends JpaRepository<Registration, Long
     List<Registration> findByBoarding(Boarding boarding);
 
     List<Registration> findByBoarding_Owner(User owner);
+    
+    Optional<Registration> findById(Long id);
     
     List<Registration> findByBoarding_IdAndStatus(Long boardingId, RegistrationStatus status);
 
