4357fec51356f65e87f210198c28dc683408bedd
Themiya <themiyakaluarachchi@gmail.com>
2025-12-22 07:26:47 +0530

    fix some issues in header in mobile view

--- front-end/src/components/student/common/StudentLayout.jsx
+++ front-end/src/components/student/common/StudentLayout.jsx
@@ -12,10 +12,10 @@ const StudentLayout = ({
     <div className="flex flex-col lg:flex-row min-h-screen bg-background-light">
       {/* Persistent Sidebar */}
       <Sidebar />
-
+      
       {/* Main Content Area */}
       <main className="flex-grow p-4 md:p-6 lg:p-8 pt-4">
-        {/* Header */}
+        {/* Header - Static on mobile, sticky on desktop */}
         <Header 
           title={title} 
           subtitle={subtitle} 
