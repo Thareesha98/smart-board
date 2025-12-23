7a3090478d86262ddfb630df9824234481cf72bc
Thareesha <thareesha98@gmail.com>
2025-12-23 23:31:06 +0530

    Add PaymentHistoryDTO

---
+++ backend/sbms-monolith/src/main/java/com/sbms/sbms_monolith/dto/payment/PaymentHistoryDTO.java
@@ -0,0 +1,30 @@
+package com.sbms.sbms_monolith.dto.payment;
+
+
+import com.sbms.sbms_monolith.model.enums.PaymentMethod;
+import com.sbms.sbms_monolith.model.enums.PaymentStatus;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+public class PaymentHistoryDTO {
+
+    private Long id;
+
+    private String transactionRef;
+
+    private BigDecimal amount;
+
+    private PaymentMethod method;
+
+    private PaymentStatus status;
+
+    private String failureReason;
+
+    private LocalDateTime paidAt;
+    
+    private String receiptUrl;
+
+}
