af754c57fbb2470a7606e1be68436b7a49ff47f1
Themiya <themiyakaluarachchi@gmail.com>
2025-12-26 16:55:52 +0530

    refactor: rename AuthContext to StudentAuthContext and update imports

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
-import { AuthProvider } from "./context/student/AuthContext";
+import { AuthProvider } from "./context/student/StudentAuthContext";
 import ScrollToTop from "./ScrollToTop";
 
 import OwnerLayout from "./layouts/OwnerLayout";
