1751a72c0a509cc267aed4561e1a93a03e7fd0af
Themiya <themiyakaluarachchi@gmail.com>
2025-12-27 19:38:08 +0530

    added UserProfileViewDTO

---
+++ backend/sbms-monolith/src/main/java/com/sbms/sbms_monolith/dto/user/UserProfileViewDTO.java
@@ -0,0 +1,37 @@
+package com.sbms.sbms_monolith.dto.user;
+
+import com.sbms.sbms_monolith.dto.boarding.BoardingSummaryDTO;
+import com.sbms.sbms_monolith.dto.report.ReportResponseDTO;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class UserProfileViewDTO {
+
+    // Common Details
+    private Long id;
+    private String firstName;
+    private String email;
+    private String phone;
+    private String avatar;
+    private String role;
+    private String gender;
+    private String joinedDate;
+
+    // --- Student Specific
+    private String university;
+
+    // ---- Owner Specific
+    private String address;
+    private boolean verifiedOwner;
+    private String businessName;
+
+    // --- History
+    private List<ReportResponseDTO> incidentHistory;
+    private int totalReportsAgainst;
+    private boolean isSuspended;
+
+    // --- Active Listing for Owners
+    private List<BoardingSummaryDTO> activeListings;
+}
