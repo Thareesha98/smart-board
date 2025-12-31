44dd09715ef3f3ac0b45b71201678ba6aca97a69
Themiya <themiyakaluarachchi@gmail.com>
2025-12-31 07:24:40 +0530

    refactor old StudentAuthContext code

--- front-end/src/context/student/StudentAuthContext.jsx
+++ front-end/src/context/student/StudentAuthContext.jsx
@@ -1,6 +1,5 @@
 import React, { createContext, useContext, useState, useEffect } from "react";
 import api from "../../api/api";
-import StudentService from "../../api/student/StudentService";
 
 const StudentAuthContext = createContext(null);
 
