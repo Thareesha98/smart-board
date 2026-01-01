183f6a31ea3e531cdb62c4d02ea8ebe7172cd7b9
DinukaVishal <8dinukavc@gmail.com>
2026-01-01 19:37:57 +0530

    created AdMapper.java file

---
+++ backend/sbms-monolith/src/main/java/com/sbms/sbms_monolith/mapper/AdMapper.java
@@ -0,0 +1,38 @@
+package com.sbms.sbms_monolith.mapper;
+
+import com.sbms.sbms_monolith.dto.ads.AdCreateDTO;
+import com.sbms.sbms_monolith.dto.ads.AdResponseDTO;
+import com.sbms.sbms_monolith.model.ThirdPartyAd;
+import com.sbms.sbms_monolith.model.enums.Status;
+
+public class AdMapper {
+
+    public static ThirdPartyAd toEntity(AdCreateDTO dto) {
+        ThirdPartyAd ad = new ThirdPartyAd();
+        ad.setTitle(dto.getTitle());
+        ad.setCompanyName(dto.getCompanyName());
+        ad.setRedirectUrl(dto.getRedirectUrl());
+        ad.setBannerImageUrl(dto.getBannerImageUrl());
+        ad.setExpiryDate(dto.getExpiryDate());
+        ad.setTargetPanels(dto.getTargetPanels());
+        ad.setPlanName(dto.getPlanName());
+        ad.setPlanPrice(dto.getPlanPrice());
+        ad.setStatus(Status.PENDING);
+        return ad;
+    }
+
+    public static AdResponseDTO toResponse(ThirdPartyAd ad) {
+        AdResponseDTO dto = new AdResponseDTO();
+        dto.setId(ad.getId());
+        dto.setTitle(ad.getTitle());
+        dto.setCompanyName(ad.getCompanyName());
+        dto.setRedirectUrl(ad.getRedirectUrl());
+        dto.setBannerImageUrl(ad.getBannerImageUrl());
+        dto.setExpiryDate(ad.getExpiryDate());
+        dto.setStatus(ad.getStatus());
+        dto.setTargetPanels(ad.getTargetPanels());
+        dto.setPlanName(ad.getPlanName());
+        dto.setCreatedAt(ad.getCreatedAt());
+        return dto;
+    }
+}
