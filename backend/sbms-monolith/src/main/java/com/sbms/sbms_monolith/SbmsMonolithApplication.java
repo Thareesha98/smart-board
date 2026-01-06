df3f2b8fe8204e0530202005dcd5537c9b07d014
Thareesha <thareesha98@gmail.com>
2026-01-06 16:59:20 +0530

    add async

--- backend/sbms-monolith/src/main/java/com/sbms/sbms_monolith/SbmsMonolithApplication.java
+++ backend/sbms-monolith/src/main/java/com/sbms/sbms_monolith/SbmsMonolithApplication.java
@@ -2,7 +2,9 @@ package com.sbms.sbms_monolith;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @SpringBootApplication
 public class SbmsMonolithApplication {
 
