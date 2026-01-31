0def6f86143684abd8e3fc5e6a700b591ba7e181
Thareesha <thareesha98@gmail.com>
2026-01-31 23:41:04 +0530

    feat: update manual payment services (Bank Slip and Cash)

--- backend/sbms-monolith/src/main/java/com/sbms/sbms_monolith/service/CashPaymentService.java
+++ backend/sbms-monolith/src/main/java/com/sbms/sbms_monolith/service/CashPaymentService.java
@@ -5,10 +5,13 @@ import java.time.LocalDateTime;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.sbms.sbms_monolith.model.MonthlyBill;
 import com.sbms.sbms_monolith.model.PaymentIntent;
 import com.sbms.sbms_monolith.model.enums.ManualApprovalStatus;
+import com.sbms.sbms_monolith.model.enums.MonthlyBillStatus;
 import com.sbms.sbms_monolith.model.enums.PaymentIntentStatus;
 import com.sbms.sbms_monolith.model.enums.PaymentMethod;
+import com.sbms.sbms_monolith.repository.MonthlyBillRepository;
 import com.sbms.sbms_monolith.repository.PaymentIntentRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -18,6 +21,8 @@ import lombok.RequiredArgsConstructor;
 public class CashPaymentService {
 
     private final PaymentIntentRepository intentRepo;
+    
+    private final MonthlyBillRepository billRepo;
 
     @Transactional
     public void createCashPayment(Long intentId) {
@@ -34,14 +39,20 @@ public class CashPaymentService {
             // idempotent — already submitted
             return;
         }
+        
+        MonthlyBill bill = billRepo.findById(intent.getMonthlyBillId())
+        .orElseThrow(() -> new RuntimeException("Payment intent not found"));
+        
+        bill.setStatus(MonthlyBillStatus.PENDING);
+        
 
-        // ✅ MARK AS SUBMITTED (NOT CONFIRMED)
+        //  MARK AS SUBMITTED (NOT CONFIRMED)
         intent.setStatus(PaymentIntentStatus.AWAITING_MANUAL_APPROVAL);
         intent.setManualApprovalStatus(ManualApprovalStatus.PENDING);
         intent.setCompletedAt(LocalDateTime.now());
         
         intent.setMethod(PaymentMethod.CASH);
-        intent.setReferenceId("CASH");
+     //   intent.setReferenceId("CASH");
 
 
         intentRepo.save(intent);
