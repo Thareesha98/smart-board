93e871e69deef57cdb31c048c882b7113c37c1ea
Themiya <themiyakaluarachchi@gmail.com>
2026-02-03 07:47:42 +0530

    add technician service functions for profile, jobs, and reporting

---
+++ front-end/src/api/technician/technicianService.js
@@ -0,0 +1,37 @@
+import api from "../api";
+
+// 1. Get Technician Profile
+export const getTechnicianProfile = async () => {
+  const response = await api.get("/technician-workflow/profile");
+  return response.data;
+};
+
+// 2. Get Assigned Jobs
+export const getTechnicianJobs = async () => {
+  const response = await api.get("/technician-workflow/my-jobs");
+  return response.data;
+};
+
+// 3. Accept or Reject Job
+export const respondToJob = async (maintenanceId, accept, reason = null) => {
+  const response = await api.put(`/technician-workflow/${maintenanceId}/decision`, null, {
+    params: { accept, reason }
+  });
+  return response.data;
+};
+
+// 4. Complete Job & Send Bill
+export const completeJob = async (maintenanceId, amount) => {
+  const response = await api.put(`/technician-workflow/${maintenanceId}/complete`, null, {
+    params: { amount }
+  });
+  return response.data;
+};
+
+// 5. Create Report (Against Owner)
+export const createTechnicianReport = async (formData) => {
+  const response = await api.post("/reports", formData, {
+    headers: { 'Content-Type': 'multipart/form-data' }
+  });
+  return response.data;
+};
