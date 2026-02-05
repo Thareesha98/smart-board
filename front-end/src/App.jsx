5e40e7707ca484ae1fc8ad6dac2edfeeee7547c2
Dinuka Vishal <dinukavishal797@gmail.com>
2026-02-05 13:37:12 +0530

    update App.jsx

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -6,10 +6,12 @@ import Home from "./Home.jsx";
 // --- CONTEXT PROVIDERS ---
 import { StudentAuthProvider } from "./context/student/StudentAuthContext.jsx";
 import { OwnerAuthProvider } from "./context/owner/OwnerAuthContext.jsx";
+import { AdminAuthProvider } from "./context/admin/AdminAuthContext.jsx";
 
 // --- ROUTE FILES ---
 import StudentAppRoutes from "./routes/StudentAppRoutes.jsx";
 import OwnerAppRoutes from "./routes/OwnerAppRoutes";
+import AdminAppRoutes from "./routes/AdminAppRoutes";
 
 // --- NEW UNIFIED PAGES ---
 // Ensure these paths match where you saved the files above
@@ -24,36 +26,41 @@ function App() {
     <>
       <StudentAuthProvider>
         <OwnerAuthProvider>
-          <ScrollToTop />
-          <Toaster
-            position="top-right"
-            toastOptions={{
-              style: {
-                background: "#333",
-                color: "#fff",
-                borderRadius: "10px",
-                fontSize: "14px",
-              },
-            }}
-          />
-          <Routes>
-            {/* ==================== GLOBAL AUTH ROUTES ==================== */}
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/signup" element={<SignupPage />} />
+          <AdminAuthProvider>
+            <ScrollToTop />
+            <Toaster
+              position="top-right"
+              toastOptions={{
+                style: {
+                  background: "#333",
+                  color: "#fff",
+                  borderRadius: "10px",
+                  fontSize: "14px",
+                },
+              }}
+            />
+            <Routes>
+              {/* ==================== GLOBAL AUTH ROUTES ==================== */}
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/signup" element={<SignupPage />} />
 
-            {/* ==================== ROLE SPECIFIC ROUTES ==================== */}
-            {/* Student routes (Dashboard, etc.) */}
-            <Route path="/student/*" element={<StudentAppRoutes />} />
+              {/* ==================== ROLE SPECIFIC ROUTES ==================== */}
+              {/* Student routes (Dashboard, etc.) */}
+              <Route path="/student/*" element={<StudentAppRoutes />} />
 
-            {/* Owner routes (Dashboard, My Ads, etc.) */}
-            <Route path="/owner/*" element={<OwnerAppRoutes />} />
+              {/* Owner routes (Dashboard, My Ads, etc.) */}
+              <Route path="/owner/*" element={<OwnerAppRoutes />} />
 
-             {/* Public Profile view */}
-            <Route path="/profile/view/:id" element={<PublicProfileView />} />
+              {/* Admin routes (Dashboard, Users, Ads, etc.) */}
+              <Route path="/admin/*" element={<AdminAppRoutes />} />
 
-            {/* ==================== LANDING PAGE ==================== */}
-            <Route path="/" element={<Home />} />
-          </Routes>
+               {/* Public Profile view */}
+              <Route path="/profile/view/:id" element={<PublicProfileView />} />
+
+              {/* ==================== LANDING PAGE ==================== */}
+              <Route path="/" element={<Home />} />
+            </Routes>
+          </AdminAuthProvider>
         </OwnerAuthProvider>
       </StudentAuthProvider>
     </>
