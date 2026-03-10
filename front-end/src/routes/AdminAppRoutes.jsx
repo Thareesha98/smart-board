61c66a2031a90009e4f7b2ed465ba0e9ca9f241a
sandun18 <sandunanjana333@gmail.com>
2026-03-10 12:54:21 +0530

    update routes

---
+++ front-end/src/routes/AdminAppRoutes.jsx
@@ -0,0 +1,30 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import AdminProtectedRoute from "../components/admin/AdminProtectedRoute.jsx";
+import AdminLayout from "../layouts/AdminLayout";
+
+// Admin Pages
+import AdminDashboard from "../pages/admin/AdminDashboard";
+import ManageSubscriptionPlansPage from "../pages/admin/ManageSubscriptionPlansPage";
+
+const AdminAppRoutes = () => {
+  return (
+    <Routes>
+      {/* ==================== PROTECTED ADMIN ROUTES ==================== */}
+      <Route element={<AdminProtectedRoute />}>
+        <Route path="/" element={<AdminLayout />}>
+          <Route index element={<AdminDashboard />} />
+          <Route path="dashboard" element={<AdminDashboard />} />
+          <Route
+            path="subscription-plans"
+            element={<ManageSubscriptionPlansPage />}
+          />
+        </Route>
+      </Route>
+
+      {/* ==================== FALLBACK ==================== */}
+      <Route path="*" element={<Navigate to="/login" replace />} />
+    </Routes>
+  );
+};
+
+export default AdminAppRoutes;
