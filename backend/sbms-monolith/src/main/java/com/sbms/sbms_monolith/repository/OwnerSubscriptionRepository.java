9128a3ce686a418874a871c01b4f5d1890378d78
sandun18 <sandunanjana333@gmail.com>
2026-03-11 08:15:49 +0530

    change 3 files

---
+++ backend/sbms-monolith/src/main/java/com/sbms/sbms_monolith/repository/OwnerSubscriptionRepository.java
@@ -0,0 +1,17 @@
+package com.sbms.sbms_monolith.repository;
+
+import com.sbms.sbms_monolith.model.OwnerSubscription;
+import com.sbms.sbms_monolith.model.enums.OwnerSubscriptionStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface OwnerSubscriptionRepository extends JpaRepository<OwnerSubscription, Long> {
+
+    List<OwnerSubscription> findByOwnerIdOrderByCreatedAtDesc(Long ownerId);
+
+    Optional<OwnerSubscription> findByOwnerIdAndStatus(Long ownerId, OwnerSubscriptionStatus status);
+
+    boolean existsByOwnerIdAndStatus(Long ownerId, OwnerSubscriptionStatus status);
+}
